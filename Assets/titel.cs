88083f3851373d6ee579f315b2504e642402cd58
hotta27 <86353892+hotta27@users.noreply.github.com>
2023-02-23 15:09:03 +0900

    networkは完成 出力を4->2に変更しよう

--- Assets/titel.cs
+++ Assets/titel.cs
@@ -17,16 +17,20 @@ public class titel : MonoBehaviour
     public Transform target;
     int t,s=500;
     Data data;
+    int nomber;
 
     void Start()
     {
         t = 0;
         rb = GetComponent<Rigidbody>();
-        
+
         data = new Data();
-        foreach (int i in data.input) data.input[i] = Random.Range(-1f, 1f);
-        foreach (int i in data.output) data.output[i] = Random.Range(-1f, 1f);
-        foreach (int i in data.w) data.w[i] = Random.Range(-1f, 1f);
+        for (int i = 0; i < data.input.Length; i++) data.input[i] = 0;
+        for (int i = 0; i < data.output.Length; i++) data.output[i] = Random.Range(-1f, 1f);
+        for (int i = 0; i < data.w.Length; i++) data.w[i] = Random.Range(-1f, 1f);
+        nomber = Random.Range(0, 5);
+
+
     }
 
     void Update()
@@ -34,9 +38,10 @@ public class titel : MonoBehaviour
         float x = 0,y=0;
         if (t == 0)
         {
-             x =Random.Range(-1f, 1f) * speed;
-             y =Random.Range(-1f, 1f) * speed;
-            
+            //outputを2次元にする。x,yを出力にする
+            x = Random.Range(-1f, 1f) * speed;
+            y = Random.Range(-1f, 1f) * speed;
+
             t = s;
         }t--;
         rb.AddForce(x, y, 0);
@@ -69,19 +74,52 @@ public class titel : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit, len))
             {
-                Debug.Log(hit.collider.gameObject.tag);
+                //Debug.Log(hit.collider.gameObject.tag);
+                switch (hit.collider.gameObject.tag)
+                {
+                    case "out":
+                        data.input[i] = 0.5f;
+                        break;
+                    case "block":
+                        data.input[i] = 1.0f;
+                        break;
+                    case "item":
+                        data.input[i] = 1.5f;
+                        break;
+                    default:
+                        data.input[i] = 2.0f;
+                        break;
+                }
             }
 
             Debug.DrawRay(ray.origin, direction, Color.red, .5f);
         }
+        Network();
     }
    
     void Network()
     {
-        float x,z;
-        foreach(int i in data.input) x = data.input[i] * data.w[i];
+        float x=0,z=0,num=0;
+
+        for (int i=0; i<data.input.Length;i++) x += data.input[i] * data.w[i];  
         z = Sigmoid(x);
-        
+        for (int i = 0; i < data.output.Length; i++)
+        {
+            data.output[i] = z * data.w[i + data.input.Length];
+            Debug.Log(data.output[i]+" "+i);
+
+            if (i == 0)
+            {
+                num = data.output[0];
+                nomber = 0;
+            }
+            else if (num < data.output[i])
+            {
+                num = data.output[i];
+                nomber = i;
+            }
+        }
+
     }
 
     float Sigmoid(float x)

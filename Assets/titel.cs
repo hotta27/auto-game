8b4516405eb8c33eb8f92b55ae37bda0657dce27
hotta27 <86353892+hotta27@users.noreply.github.com>
2023-03-22 00:34:13 +0900

    センサーの変更

--- Assets/titel.cs
+++ Assets/titel.cs
@@ -13,10 +13,11 @@ public class titel : MonoBehaviour
     public Transform goal;
     public bool wb=true;
     int t,s=500,input,output,w;
-    float n=60,pp=0;
+    float n=30,pp=0;
     Data data;
     savedata save;
     GameObject x;
+    Vector3 origin;
 
     void Start()
     {
@@ -36,7 +37,7 @@ public class titel : MonoBehaviour
         }else{
             data=save.Jload("savedata");
             if(wb){
-                Debug.Log("wwwww");
+                //Debug.Log("wwwww");
                 //重み変更
                 for(int i=0;i<data.w.Length;i++){
                     data.w[i]+=Random.Range(-0.5f,0.5f);
@@ -49,6 +50,7 @@ public class titel : MonoBehaviour
         input=data.input.Length;
         output=data.output.Length;
         w=data.w.Length;
+
         
     }
 
@@ -62,8 +64,8 @@ public class titel : MonoBehaviour
              x = Random.Range(-1f, 1f) * speed;
              y = Random.Range(-1f, 1f) * speed;
             }else{
-            x=data.output[0] * speed;
-            y=data.output[1] * speed;
+                x = data.output[0] * speed;
+                y = data.output[1] * speed;
             }
         
             t = s;
@@ -71,61 +73,43 @@ public class titel : MonoBehaviour
         rb.AddForce(x, y, 0);
         ca.transform.position = new Vector3(p.transform.position.x, p.transform.position.y + 1, -10);
 
-      
-      Vector3 origin = this.transform.position; // 原点
 
-        int vx=10, vy=0;
-        for (int i = 0; i < 4; i++)
-        {
-           
-            switch (i)
-            {
-                case 1:
-                    vx = -10;
-                    break;
-                case 2:
-                    vx = 0;
-                    vy = 10;
-                    break;
-                case 3:
-                    vy = -10;
-                    break;
-            }
-            Vector3 direction = new Vector3(vx, vy, 0); // X軸方向を表すベクトル
-            Ray ray = new Ray(origin: origin, direction: direction); // Rayを生成
-            RaycastHit hit;
-            
-            //Rayの衝突判定
-            if (Physics.Raycast(ray, out hit, 10f))
-            {
-                
-                //Debug.Log(hit.collider.gameObject.tag);
-                switch (hit.collider.gameObject.tag)
-                {
-                    case "out":
-                        data.input[i] = 0.5f;
-                        data.point-=0.01f;
-                        break;
-                    case "block":
-                        data.input[i] = 1.0f;
-                        data.point+=0.01f;
-                        break;
-                    case "item":
-                        data.input[i] = 1.5f;
-                         data.point+=0.02f;
-                        break;
-                    default:
-                        data.input[i] = 2.0f;
-                        break;
-                }
-                data.input[i+4]=Vector3.Distance(origin,hit.transform.position);
-                
-                //Debug.Log(data.input[i+4]+" "+i);
-            }
+        origin = this.transform.position; // 原点
 
-            Debug.DrawRay(ray.origin, direction, Color.red, .5f);
-        }
-        data.point-=0.0001f;
+        //  int vx=10, vy=0;
+        //  for (int i = 0; i < 4; i++)
+        //  {
+
+        //      switch (i)
+        //      {
+        //          case 1:
+        //              vx = -10;
+        //              break;
+        //          case 2:
+        //              vx = 0;
+        //              vy = 10;
+        //              break;
+        //          case 3:
+        //              vy = -10;
+        //              break;
+        //      }
+        //      Vector3 direction = new Vector3(vx, vy, 0); // X軸方向を表すベクトル
+        //      Ray ray = new Ray(origin: origin, direction: direction); // Rayを生成
+        //      RaycastHit hit;
+
+        //      //Rayの衝突判定
+        //      if (Physics.Raycast(ray, out hit, 10f))
+        //      {
+
+        //          //Debug.Log(hit.collider.gameObject.tag);
+
+
+        //          //Debug.Log(data.input[i+4]+" "+i);
+        //      }
+
+        //      Debug.DrawRay(ray.origin, direction, Color.red, .5f);
+        //  }
+        //data.point-=0.0001f;
         data.input[data.input.Length-1]=Vector3.Distance(origin,goal.position);
         //Debug.Log(data.input[data.input.Length-1]);
 
@@ -137,7 +121,7 @@ public class titel : MonoBehaviour
         else if(data.input[data.input.Length-1]<=10f) data.point+=10f;
         else if(data.input[data.input.Length-1]<=15f) data.point=1f;
 
-       finsh();
+        finsh();
         }
         else n-=0.001f;
         //Debug.Log(n);
@@ -145,20 +129,24 @@ public class titel : MonoBehaviour
    
     void Network()
     {
-        int N=1,net;
+        int N=1,M=1,j,k=0;
         float x=0,z=0;
-        float[] xx=new float[N],zz=new float[N];
+        float[,] xx=new float[N,M],zz=new float[N,M];
         
         for (int i=0; i<data.input.Length;i++) x += data.input[i] * data.w[i];  
         z = Sigmoid(x);
-        for(net=0;net<N;net++){
-            xx[net]=0;
-            xx[net]+=z*data.w[input+1];
-            zz[net]=Sigmoid(xx[net]);
+        for (j = 0; j < N; j++)
+        {
+            for (k = 0; k < M; k++)
+            {
+                xx[j,k] = 0;
+                xx[j,k] += z * data.w[input + 1];
+                zz[j,k] = Sigmoid(xx[j,k]);
+            }
         }
         for (int i = 0; i < data.output.Length; i++)
         {
-            data.output[i] = zz[net-1] * data.w[i + data.input.Length + N];
+            data.output[i] = zz[j-1,k-1] * data.w[i + data.input.Length + N];
             //Debug.Log(data.output[i]+" "+i);         
         }
 
@@ -172,9 +160,8 @@ public class titel : MonoBehaviour
 
     void finsh (){
         Debug.Log(data.point+" <-point");
-        data.move-=0.01f;
         if(pp<data.point || Random.Range(0f,1f)<=data.move) {
-            data.move-=0.02f;
+            data.move-=0.01f;
             save.Jsave(data,"savedata"); 
             Debug.Log("grow up");
             }
@@ -184,9 +171,34 @@ public class titel : MonoBehaviour
     {
         if (other.gameObject.tag == "goal")
         {
+            Debug.Log("goal");
             data.point+=100f;
            finsh();
         }
+
+        int i = 0;
+        switch (other.gameObject.tag)
+        {
+            case "out":
+                data.input[i] = 0.5f;
+                data.point -= 0.001f;
+                break;
+            case "block":
+                data.input[i] = 1.0f;
+                data.point += 0.001f;
+                break;
+            case "item":
+                data.input[i] = 1.5f;
+                data.point += 0.002f;
+                break;
+            default:
+                data.input[i] = 2.0f;
+                break;
+        }
+        data.input[1] = other.transform.position.x;
+        data.input[2] = other.transform.position.y;
+        //Debug.Log(data.input[1] + " "+"\n"+ data.input[2]+other.gameObject.tag);
+        data.input[3] = Vector3.Distance(origin, other.transform.position);
     }
 
     void OnCollisionEnter(Collision collision)
